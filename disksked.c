31fc50e6d24d5fe8d2f534c74484cadcd6b12c4b
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2018-04-06 10:18:51 -0600

    Commented out print diagnostics and tested against Cucumber

--- disksked.c
+++ disksked.c
@@ -22,35 +22,32 @@ int main(int argc, char *argv[]){
     int distance[9];
     int Total = 0;
 
-
-    printf("Start is at: %i\n",Start);
+    //printf("Start is at: %i\n",Start);
     for(int i = 0; i < 8; i++) {
         tracks[i] = atoi(argv[i + 2]);
-        printf("Loaded Track: %i\n",tracks[i]);
+        //printf("Loaded Track: %i\n",tracks[i]);
     }
 
     distance[0]  = abs(Start - tracks[0]);
-    printf("Initial starting distance: %i\n", distance[0]);
+    //printf("Initial starting distance: %i\n", distance[0]);
 
     for(int i = 1; i < 9; i++) {
-        printf("Comparing Tracks: %i and %i\n",tracks[i], tracks[i+1]);
+        //printf("Comparing Tracks: %i and %i\n",tracks[i], tracks[i+1]);
         distance[i] = abs(tracks[i-1] - tracks[i]);
-        printf("Distance right now: %i\n", distance[i]);
+        //printf("Distance right now: %i\n", distance[i]);
     }
 
     for(int i = 0; i < 8; i++) {
         Total += distance[i];
-        printf("Total right now: %i\n", Total);
+        //printf("Total right now: %i\n", Total);
     }
 
+    //FCFS Print Function
     printf("FCFS: Start:%i", Start);
     for(int i = 0; i < 8; i++){
         printf(" %i:%i", tracks[i], distance[i]);
     }
     printf(" Total:%i\n", Total);
-
-    /*printf("FCFS: Start:%i %i:%i %i:%i %i:%i %i:%i %i:%i %i:%i %i:%i %i:%i Total:%i\n",
-           Start, T1, D1, T2, D2, T3, D3, T4, D4, T5, D5, T6, D6, T7, D7, T8, D8, Total);*/
 
     return 0;
 }
