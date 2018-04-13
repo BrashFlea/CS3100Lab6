b9ae199f5f8700f2a674f3d89857f3553f616752
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2018-04-13 13:44:14 -0600

    Completed C_Scan algorithm and tested. Also removed lots of diagnostic messages from FCFS.

--- disksked.c
+++ disksked.c
@@ -22,40 +22,28 @@ int main(int argc, char *argv[]){
     int distance[9];
     int Total = 0;
 
-    /*int sstf_tracks[8];
-    int sstf_tracks_ordered[8];
-    int sstf_distance = 0;
-    int sstf_current_dist = 257;
-    int sstf_next = 0;
-    int sstf_start = -1;*/
-
-    int scan_tracks[9];
+    int scan_tracks[8];
     int scan_total = 0;
-    int scan_distance[8];
-    int scan_counter = 8;
-    int scan_current_dist = 257;
-    int scan_next = 0;
     int scan_index = -1;
     int temp = 0;
 
-    //printf("Start is at: %i\n",Start);
+    int cscan_tracks[8];
+    int cscan_total = 0;
+    int cscan_index = -3;
+    int ctemp = 0;
+
+    //FCFS
     for(int i = 0; i < 8; i++) {
         tracks[i] = atoi(argv[i + 2]);
-        //printf("Loaded Track: %i\n",tracks[i]);
     }
-
     distance[0]  = abs(Start - tracks[0]);
-    //printf("Initial starting distance: %i\n", distance[0]);
 
     for(int i = 1; i < 9; i++) {
-        //printf("Comparing Tracks: %i and %i\n",tracks[i-1], tracks[i]);
         distance[i] = abs(tracks[i-1] - tracks[i]);
-        //printf("Distance right now: %i\n", distance[i]);
     }
 
     for(int i = 0; i < 8; i++) {
         Total += distance[i];
-        //printf("Total right now: %i\n", Total);
     }
 
     //FCFS Print Function
@@ -65,43 +53,14 @@ int main(int argc, char *argv[]){
     }
     printf(" Total:%i\n", Total);
 
-
-    //SSTF WIP
-    /*for (int i = 0; i < 8; i++) {
-        sstf_tracks[i] = tracks[i];
-    }
-
-    printf("Start track: %i\n", Start);
-    for (int i = 0; i < 8; i++) {
-        sstf_distance = abs(Start - sstf_tracks[i]);
-        if (sstf_distance < sstf_current_dist){
-            sstf_current_dist = sstf_distance;
-            sstf_next = sstf_tracks[i];
-        }
-    }
-    printf("Next track to move to: %i\n", sstf_next);
-
-    for (int j = 0; j < 8; j++){
-        printf("Currently on track: %i\n", sstf_tracks[j]);
-        for (int i = 0; i < 8; i++) {
-            sstf_distance = abs(sstf_tracks[j] - sstf_tracks[i]);
-            //printf("Comparing tracks: %i and %i\n", sstf_tracks[j], sstf_tracks[i]);
-            if (sstf_distance < sstf_current_dist){
-                sstf_current_dist = sstf_distance;
-                sstf_next = sstf_tracks[j];
-            }
-
-        }
-        //printf("Next track to move to: %i\n", sstf_next);
-        sstf_tracks_ordered[j] = sstf_next;
-    }*/
-
+    //SCAN
     printf("SCAN: Start:%i", Start);
 
     for (int i = 0; i < 8; i++) {
         scan_tracks[i] = tracks[i];
     }
 
+    //Sort tracks
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 7; j++) {
             if(scan_tracks[j] > scan_tracks[j+1]){
@@ -136,14 +95,15 @@ int main(int argc, char *argv[]){
         }
         //Wrap around to the beginning of the array
         printf("%i:%i ", scan_tracks[0], abs(scan_tracks[0] - scan_tracks[7]));
-        //Print values from the start to the index
         scan_total += abs(scan_tracks[0] - scan_tracks[7]);
+        //Process values from the start to the index
         for (int i = 1; i < scan_index; i++) {
             printf("%i:%i ", scan_tracks[i], abs(scan_tracks[i-1] - scan_tracks[i]));
             scan_total += abs(scan_tracks[i-1] - scan_tracks[i]);
         }
         printf("Total:%i\n", scan_total);
     } else {
+        //Start is at beginning of array
         printf(" %i:%i ", scan_tracks[0], abs(Start - scan_tracks[0]));
         scan_total += abs(Start - scan_tracks[0]);
         for (int i = 1; i < 8; i++) {
@@ -153,8 +113,86 @@ int main(int argc, char *argv[]){
         printf("Total:%i\n", scan_total);
     }
 
+    //CSCAN
+    printf("C_SCAN: Start:%i", Start);
+
+    for (int i = 0; i < 8; i++) {
+        cscan_tracks[i] = tracks[i];
+    }
+
+    //Sort tracks
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 7; j++) {
+            if(cscan_tracks[j] > cscan_tracks[j+1]){
+                ctemp = cscan_tracks[j];
+                cscan_tracks[j] = cscan_tracks[j + 1];
+                cscan_tracks[j + 1] = ctemp;
+            }
+        }
+    }
+
+    /*printf("\ncscan_tracks: ");
+    for (int i = 0; i < 8; i++) {
+        printf("%i:%i ", i, cscan_tracks[i]);
+    }
+    printf("\n");*/
+
+    if(Start > cscan_tracks[7]) {
+        cscan_index = -1;
+    } else if(Start < cscan_tracks[7]) {
+        cscan_index = -2;
+    }
+
+    for (int i = 1; i < 8; i++) {
+        if((cscan_tracks[i-1] < Start) && (Start <= cscan_tracks[i])){
+            cscan_index = i;
+        }
+    }
+    //printf("cscan_index is: %i\n",cscan_index);
+
+    if (cscan_index == -1) {
+        //Process from the end of the array to the beginning
+        printf(" %i:%i ", cscan_tracks[7], abs(Start - cscan_tracks[7]));
+        cscan_total += abs(Start - cscan_tracks[7]);
+        for (int i = 6; i >= 0; i--) {
+            printf("%i:%i ", cscan_tracks[i], abs(cscan_tracks[i + 1] - cscan_tracks[i]));
+            cscan_total += abs(cscan_tracks[i + 1] - cscan_tracks[i]);
+        }
+        printf("Total:%i\n", cscan_total);
+    }
+
+    if (cscan_index == -2) {
+        //Process the beginning of array
+        printf(" %i:%i ", cscan_tracks[0], abs(Start - cscan_tracks[0]));
+        cscan_total += abs(Start - cscan_tracks[0]);
+        for (int i = 1; i < 8; i++) {
+            printf("%i:%i ", cscan_tracks[i], abs(cscan_tracks[i - 1] - cscan_tracks[i]));
+            cscan_total += abs(cscan_tracks[i - 1] - cscan_tracks[i]);
+        }
+        printf("Total:%i\n", cscan_total);
+
+    }
+
+    else if (cscan_index != -1) {
+        //Collect the Start and first index values
+        printf(" %i:%i ", cscan_tracks[cscan_index], abs(Start - cscan_tracks[cscan_index]));
+        //Traverse values higher than the index
+        cscan_total += abs(Start - cscan_tracks[cscan_index]);
+        for (int i = (cscan_index + 1); i < 8; i++) {
+            printf("%i:%i ", cscan_tracks[i], abs(cscan_tracks[i - 1] - cscan_tracks[i]));
+            cscan_total += abs(cscan_tracks[i - 1] - cscan_tracks[i]);
+        }
+        //Wrap around to the beginning of the array
+        printf("%i:%i ", scan_tracks[cscan_index - 1], abs(scan_tracks[cscan_index - 1] - scan_tracks[7]));
+        cscan_total += abs(cscan_tracks[cscan_index - 1] - cscan_tracks[7]);
+        //Process smaller values in descending order
+        for (int i = cscan_index - 2; i >= 0; i--) {
+            printf("%i:%i ", cscan_tracks[i], abs(cscan_tracks[i + 1] - cscan_tracks[i]));
+            cscan_total += abs(cscan_tracks[i + 1] - cscan_tracks[i]);
+        }
+        printf("Total:%i\n", cscan_total);
+    }
 
 
-    
-return 0;
+    return 0;
 }
